0e4aaec9fc1931918dd3aee5976b3c3e731fa023
Tiago Henrique Castilhos <tiago.castilhos@cyncly.com>
2025-10-03 17:25:31 -0300

    testes pagamento

--- tests/Coldmart.Pagamentos.Domain.Tests/PagamentoTests.cs
+++ tests/Coldmart.Pagamentos.Domain.Tests/PagamentoTests.cs
@@ -22,4 +22,57 @@ public class PagamentoTests
         //act & assert
         Assert.Throws<ArgumentNullException>(() => new Pagamento(null!));
     }
+
+    [Theory, AutoDomainData]
+    public void AprovarPagamento_PagamentoPendente_DeveAprovar(DadosCartao dadosCartao)
+    {
+        //arrange
+        var pagamento = new Pagamento(dadosCartao);
+
+        //act
+        pagamento.Aprovar();
+
+        //assert
+        Assert.Equal(StatusPagamento.Aprovado, pagamento.Status);
+        Assert.NotEqual(DateTimeOffset.MinValue, pagamento.DataAtualizacao);
+    }
+
+    [Theory, AutoDomainData]
+    public void CancelarPagamento_PagamentoPendente_DeveCancelar(DadosCartao dadosCartao)
+    {
+        //arrange
+        var pagamento = new Pagamento(dadosCartao);
+
+        //act
+        pagamento.Cancelar();
+
+        //assert
+        Assert.Equal(StatusPagamento.Cancelado, pagamento.Status);
+        Assert.NotEqual(DateTimeOffset.MinValue, pagamento.DataAtualizacao);
+    }
+
+    [Theory, AutoDomainData]
+    public void RecusarPagamento_PagamentoPendente_DeveRecusar(DadosCartao dadosCartao)
+    {
+        //arrange
+        var pagamento = new Pagamento(dadosCartao);
+
+        //act
+        pagamento.Recusar();
+
+        //assert
+        Assert.Equal(StatusPagamento.Recusado, pagamento.Status);
+        Assert.NotEqual(DateTimeOffset.MinValue, pagamento.DataAtualizacao);
+    }
+
+    [Theory, AutoDomainData]
+    public void AprovarPagamento_PagamentoNaoPendente_DeveLancarExcecao(DadosCartao dadosCartao)
+    {
+        //arrange
+        var pagamento = new Pagamento(dadosCartao);
+        pagamento.Recusar();
+
+        //act & assert
+        Assert.Throws<InvalidOperationException>(pagamento.Aprovar);
+    }
 }
